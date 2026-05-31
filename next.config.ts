2e21ace1516c36cb65127adf8980c18de92ced64
Dmitriy <mityay72@Dmitriys-MacBook-Pro.local>
2026-05-31 22:34:09 +0500

    fix: no-cache for auth pages

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["192.168.1.137"],
+  async headers() {
+    return [
+      {
+        source: "/auth/:path*",
+        headers: [
+          { key: "Cache-Control", value: "no-store, must-revalidate" },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
